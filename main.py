8d2c1acc47aeae3b4a61129d28258ad881b41da1
Mateo Arenas Salas <masprime77@gmail.com>
2025-04-07 23:28:20 +0200

    Second iteration All the headers processed

--- main.py
+++ main.py
@@ -3,11 +3,30 @@ data = []
 with open("data.txt", "r") as f:
     lines = f.readlines()
 
-print(lines)
-
 # strip() removes the unnecessary '\n' and spaces.
 # the code also removes the comment.
 lines = [line.strip() for line in lines if line.strip() and not line.startswith("#")]
 
-print(lines)
+headers = lines[0].split()
 
+for line in lines[1:]:
+    parts = line.split()
+
+    date = ' '.join(parts[0:4])
+    duration = parts[4:6]
+    tipo = parts[6]
+    comment = parts[7].strip('"')
+    status = ' '.join(parts[8])
+
+    entry = {
+        "date": date,
+        "duration": duration,
+        "tipo": tipo,
+        "comment": comment,
+        "status": status
+    }
+
+    data.append(entry)
+
+for entry in data:
+    print(entry)
